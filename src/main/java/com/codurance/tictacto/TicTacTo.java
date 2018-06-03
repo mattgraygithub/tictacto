514a3143e897c5da4dbe9c74e4a2e8fef2769ab5
mattgraygithub <mattgray83@hotmail.co.uk>
2018-06-03 21:19:21 +0100

    Tic Tac To Kata

--- src/main/java/com/codurance/tictacto/TicTacTo.java
+++ src/main/java/com/codurance/tictacto/TicTacTo.java
@@ -1,21 +1,23 @@
 package com.codurance.tictacto;
 
-/**
- * Tic Tac To!
- */
-
 public class TicTacTo {
-    int turn = 1;
-    String[][] board = new String[3][3];
+    private int turn = 1;
+    private String[][] board;
+    private static final String X = "X";
+    private static final String O = "O";
+
+    public TicTacTo(String[][] board) {
+        this.board = board;
+    }
 
     public String[][] nextTurn(String player, int x, int y) {
 
         if (turn == 1 && player == "O") {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("X goes first!");
         }
 
         if (board[x][y] != null) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("That space is already taken!");
         }
         board[x][y] = player;
         turn++;
@@ -23,22 +25,24 @@ public class TicTacTo {
         return board;
     }
 
-    public String CheckForWinner() {
+    public String checkForWinner() {
         String winner = "";
+        //Check Horizontal
         for (int x = 0; x < 3; x++) {
-            if (board[x][0]=="X" && board[x][1]=="X" && board[x][2]=="X"){
-                winner = "X Wins!";
+            if (board[x][0] == "X" && board[x][1] == "X" && board[x][2] == "X") {
+                winner = X;
                 break;
-            } else if (board[x][0]=="O" && board[x][1]=="O" && board[x][2]=="O"){
-                winner = "O Wins!";
+            } else if (board[x][0] == "O" && board[x][1] == "O" && board[x][2] == "O") {
+                winner = O;
                 break;
             } else {
+                //Check Vertical
                 for (int y = 0; y < 3; y++) {
-                    if (board[0][y]=="X" && board[1][y]=="X" && board[2][y]=="X"){
-                        winner = "X Wins!";
+                    if (board[0][y] == "X" && board[1][y] == "X" && board[2][y] == "X") {
+                        winner = X;
                         break;
                     } else if (board[0][y] == "O" && board[1][y] == "O" && board[2][y] == "O") {
-                        winner = "O Wins!";
+                        winner = O;
                         break;
                     } else {
                         winner = null;
@@ -46,14 +50,15 @@ public class TicTacTo {
                 }
             }
         }
+        //Check Diagonal
         if (board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") {
-            winner = "X Wins!";
+            winner = X;
         } else if (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O") {
-            winner = "O Wins!";
+            winner = O;
         } else if (board[2][0] == "X" && board[1][1] == "X" && board[0][2] == "X") {
-            winner = "X Wins!";
-        }else if (board[2][0] == "O" && board[1][1] == "O" && board[0][2] == "O") {
-            winner = "O Wins!";
+            winner = X;
+        } else if (board[2][0] == "O" && board[1][1] == "O" && board[0][2] == "O") {
+            winner = O;
         }
         return winner;
     }
--- src/test/java/com/codurance/tictacto/TicTacToTest.java
+++ src/test/java/com/codurance/tictacto/TicTacToTest.java
@@ -1,31 +1,35 @@
 package com.codurance.tictacto;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple TicTacTo.
- */
 public class TicTacToTest {
+
+    private static final String EMPTY = null;
+    private String[][] emptyBoard = new String[][]{
+            {EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY}
+    };
+
     @Test(expected = IllegalArgumentException.class)
     public void shouldNotAllowZeroToBePlayedFirst() {
-        TicTacTo tictacto = new TicTacTo();
+        TicTacTo tictacto = new TicTacTo(emptyBoard);
         tictacto.nextTurn("O", 0, 0);
     }
 
     @Test
     public void shouldAllowXToGoFirst() {
-        TicTacTo tictacto = new TicTacTo();
+        TicTacTo tictacto = new TicTacTo(emptyBoard);
         String[][] board = tictacto.nextTurn("X", 0, 0);
         assertThat(board[0][0], is("X"));
     }
 
     @Test
     public void shouldAllowZeroToGoNext() {
-        TicTacTo tictacto = new TicTacTo();
+        TicTacTo tictacto = new TicTacTo(emptyBoard);
         tictacto.nextTurn("X", 0, 0);
         String[][] board = tictacto.nextTurn("O", 0, 1);
         assertThat(board[0][1], is("O"));
@@ -33,193 +37,170 @@ public class TicTacToTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldOnlyAllowMoveIntoEmptySpace() {
-        TicTacTo tictacto = new TicTacTo();
+        TicTacTo tictacto = new TicTacTo(emptyBoard);
         tictacto.nextTurn("X", 0, 2);
         String[][] board = tictacto.nextTurn("O", 0, 2);
     }
 
     @Test
     public void xWinsHorizontalRowOne() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 0, 0);
-        tictacto.nextTurn("O", 1, 0);
-        tictacto.nextTurn("X", 0, 1);
-        tictacto.nextTurn("O", 1, 1);
-        tictacto.nextTurn("X", 0, 2);
-        assertThat(tictacto.CheckForWinner(), is("X Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {"X", "X", "X"},
+                {EMPTY, "O", "O",},
+                {EMPTY, EMPTY, EMPTY}
+        });
+        assertThat(tictacto.checkForWinner(), is("X"));
     }
 
     @Test
     public void xWinsHorizontalRowTwo() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 1, 0);
-        tictacto.nextTurn("O", 0, 0);
-        tictacto.nextTurn("X", 1, 1);
-        tictacto.nextTurn("O", 0, 1);
-        tictacto.nextTurn("X", 1, 2);
-        assertThat(tictacto.CheckForWinner(), is("X Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {"O", "O", EMPTY},
+                {"X", "X", "X"},
+                {EMPTY, EMPTY, EMPTY}
+        });
+        assertThat(tictacto.checkForWinner(), is("X"));
     }
 
     @Test
     public void xWinsHorizontalRowThree() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 2, 0);
-        tictacto.nextTurn("O", 0, 0);
-        tictacto.nextTurn("X", 2, 1);
-        tictacto.nextTurn("O", 0, 1);
-        tictacto.nextTurn("X", 2, 2);
-        assertThat(tictacto.CheckForWinner(), is("X Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {EMPTY, EMPTY, EMPTY},
+                {"O", "O", EMPTY},
+                {"X", "X", "X"}
+        });
+        assertThat(tictacto.checkForWinner(), is("X"));
     }
 
     @Test
     public void oWinsHorizontalRowOne() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 1, 0);
-        tictacto.nextTurn("O", 0, 0);
-        tictacto.nextTurn("X", 1, 1);
-        tictacto.nextTurn("O", 0, 1);
-        tictacto.nextTurn("X", 2, 1);
-        tictacto.nextTurn("O", 0, 2);
-        assertThat(tictacto.CheckForWinner(), is("O Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {"O", "O", "O"},
+                {"X", "X", EMPTY},
+                {"X", "X", EMPTY}
+        });
+        assertThat(tictacto.checkForWinner(), is("O"));
     }
 
     @Test
     public void oWinsHorizontalRowTwo() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 0, 0);
-        tictacto.nextTurn("O", 1, 0);
-        tictacto.nextTurn("X", 0, 1);
-        tictacto.nextTurn("O", 1, 1);
-        tictacto.nextTurn("X", 2, 1);
-        tictacto.nextTurn("O", 1, 2);
-        assertThat(tictacto.CheckForWinner(), is("O Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {"X", "X", EMPTY},
+                {"O", "O", "O"},
+                {"X", "X", EMPTY}
+        });
+        assertThat(tictacto.checkForWinner(), is("O"));
     }
 
     @Test
     public void oWinsHorizontalRowThree() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 0, 0);
-        tictacto.nextTurn("O", 2, 0);
-        tictacto.nextTurn("X", 0, 1);
-        tictacto.nextTurn("O", 2, 1);
-        tictacto.nextTurn("X", 1, 1);
-        tictacto.nextTurn("O", 2, 2);
-        assertThat(tictacto.CheckForWinner(), is("O Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {"X", "X", EMPTY},
+                {"X", "X", EMPTY},
+                {"O", "O", "O"}
+        });
+        assertThat(tictacto.checkForWinner(), is("O"));
     }
 
 
     @Test
     public void xWinsVerticalColumnOne() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 0, 0);
-        tictacto.nextTurn("O", 0, 1);
-        tictacto.nextTurn("X", 1, 0);
-        tictacto.nextTurn("O", 1, 1);
-        tictacto.nextTurn("X", 2, 0);
-        assertThat(tictacto.CheckForWinner(), is("X Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {"X", EMPTY, EMPTY},
+                {"X", EMPTY, EMPTY},
+                {"X", "O", "O"}
+        });
+        assertThat(tictacto.checkForWinner(), is("X"));
     }
 
     @Test
     public void xWinsVerticalColumnTwo() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 0, 1);
-        tictacto.nextTurn("O", 0, 0);
-        tictacto.nextTurn("X", 1, 1);
-        tictacto.nextTurn("O", 1, 0);
-        tictacto.nextTurn("X", 2, 1);
-        assertThat(tictacto.CheckForWinner(), is("X Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {EMPTY, "X", EMPTY},
+                {"O", "X", EMPTY},
+                {"O", "X", EMPTY}
+        });
+        assertThat(tictacto.checkForWinner(), is("X"));
     }
 
     @Test
     public void xWinsVerticalColumnThree() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 0, 2);
-        tictacto.nextTurn("O", 0, 0);
-        tictacto.nextTurn("X", 1, 2);
-        tictacto.nextTurn("O", 1, 0);
-        tictacto.nextTurn("X", 2, 2);
-        assertThat(tictacto.CheckForWinner(), is("X Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {EMPTY, EMPTY, "X"},
+                {"O", EMPTY, "X"},
+                {"O", EMPTY, "X"}
+        });
+        assertThat(tictacto.checkForWinner(), is("X"));
     }
 
     @Test
     public void oWinsVerticalColumnOne() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 1, 1);
-        tictacto.nextTurn("O", 0, 0);
-        tictacto.nextTurn("X", 2, 1);
-        tictacto.nextTurn("O", 1, 0);
-        tictacto.nextTurn("X", 2, 2);
-        tictacto.nextTurn("O", 2, 0);
-        assertThat(tictacto.CheckForWinner(), is("O Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {"O", "X", EMPTY},
+                {"O", "X", "X"},
+                {"O", EMPTY, "X"}
+        });
+        assertThat(tictacto.checkForWinner(), is("O"));
     }
 
     @Test
     public void oWinsVerticalColumnTwo() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 1, 0);
-        tictacto.nextTurn("O", 0, 1);
-        tictacto.nextTurn("X", 2, 0);
-        tictacto.nextTurn("O", 1, 1);
-        tictacto.nextTurn("X", 2, 2);
-        tictacto.nextTurn("O", 2, 1);
-        assertThat(tictacto.CheckForWinner(), is("O Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {EMPTY, "O", EMPTY},
+                {"X", "O", "X"},
+                {"X", "O", "X"}
+        });
+        assertThat(tictacto.checkForWinner(), is("O"));
     }
 
     @Test
     public void oWinsVerticalColumnThree() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 1, 0);
-        tictacto.nextTurn("O", 0, 2);
-        tictacto.nextTurn("X", 2, 0);
-        tictacto.nextTurn("O", 1, 2);
-        tictacto.nextTurn("X", 1, 1);
-        tictacto.nextTurn("O", 2, 2);
-        assertThat(tictacto.CheckForWinner(), is("O Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {"X", "X", "O"},
+                {EMPTY, "X", "O"},
+                {"X", EMPTY, "O"}
+        });
+        assertThat(tictacto.checkForWinner(), is("O"));
     }
 
     @Test
     public void xWinsDiagonalTopLeftToBottomRight() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 0, 0);
-        tictacto.nextTurn("O", 1, 0);
-        tictacto.nextTurn("X", 1, 1);
-        tictacto.nextTurn("O", 0, 1);
-        tictacto.nextTurn("X", 2, 2);
-        assertThat(tictacto.CheckForWinner(), is("X Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {"X", EMPTY, "O"},
+                {EMPTY, "X", "O"},
+                {EMPTY, EMPTY, "X"}
+        });
+        assertThat(tictacto.checkForWinner(), is("X"));
+    }
+
+
+    @Test
+    public void xWinsDiagonalBottomLeftToTopRight() {
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {"O", "O", "X"},
+                {EMPTY, "X", EMPTY},
+                {"X", EMPTY, EMPTY}
+        });
+        assertThat(tictacto.checkForWinner(), is("X"));
     }
 
     @Test
     public void oWinsDiagonalTopLeftToBottomRight() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 1, 0);
-        tictacto.nextTurn("O", 0, 0);
-        tictacto.nextTurn("X", 0, 1);
-        tictacto.nextTurn("O", 1, 1);
-        tictacto.nextTurn("X", 2, 1);
-        tictacto.nextTurn("O", 2, 2);
-        assertThat(tictacto.CheckForWinner(), is("O Wins!"));
-    }
-
-    @Test
-    public void xWinsDiagonalBottomLeftToTopRight() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 2, 0);
-        tictacto.nextTurn("O", 1, 0);
-        tictacto.nextTurn("X", 1, 1);
-        tictacto.nextTurn("O", 0, 1);
-        tictacto.nextTurn("X", 0, 2);
-        assertThat(tictacto.CheckForWinner(), is("X Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {"O", "X", "X"},
+                {"X", "O", "X"},
+                {EMPTY, EMPTY, "O"}
+        });
+        assertThat(tictacto.checkForWinner(), is("O"));
     }
 
     @Test
     public void oWinsDiagonalBottomLeftToTopRight() {
-        TicTacTo tictacto = new TicTacTo();
-        tictacto.nextTurn("X", 1, 0);
-        tictacto.nextTurn("O", 2, 0);
-        tictacto.nextTurn("X", 0, 1);
-        tictacto.nextTurn("O", 1, 1);
-        tictacto.nextTurn("X", 2, 1);
-        tictacto.nextTurn("O", 0, 2);
-        assertThat(tictacto.CheckForWinner(), is("O Wins!"));
+        TicTacTo tictacto = new TicTacTo(new String[][]{
+                {"X", "X", "O"},
+                {EMPTY, "O", "X"},
+                {"O", EMPTY, "X"}
+        });
+        assertThat(tictacto.checkForWinner(), is("O"));
     }
 }
